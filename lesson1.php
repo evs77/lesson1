21811b1678bc0eaba262d1c17d985c0c27692697
User Name <user@email.com>
2017-11-19 16:10:03 +0300

    lesson.php

---
+++ lesson1.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+
+$one = 25;
+$two = 12;
+
+echo $one + $two ."<br>";
+echo $one - $two ."<br>";
+echo $one * $two ."<br>";
+echo $one / $two ."<br>";
+echo $one % $two ."<br>";
+echo $one ** $two ."<br>";
+*/
+
+/*
+$a = 25;
+$b = 70;
+$c = 100;
+
+if ( (($a > 26) && ($a < $b)) || (($a==25) && ($c==100) && ($b==70)) ) {
+    echo "ok";
+} else {
+    echo "don`t";
+}
+*/
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
